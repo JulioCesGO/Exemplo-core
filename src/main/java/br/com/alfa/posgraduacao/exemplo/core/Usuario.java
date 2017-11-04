02c29fc18e3d68a98a39bc459384ecade96e150d
Unknown <julio@megasoftgyn.com.br>
2017-11-04 19:56:36 -0200

    #1 commit inicial

---
+++ src/main/java/br/com/alfa/posgraduacao/exemplo/core/Usuario.java
@@ -0,0 +1,31 @@
+package br.com.alfa.posgraduacao.exemplo.core;
+
+public class Usuario {
+
+	
+	private Long codigo;
+	private String usuario;
+	private String email;
+	
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
